7e2bcf23ab4a7aa3969faf66392f3f2b1f2cf712
Piotr Banachewicz <pio.banachewicz@gmail.com>
2022-05-28 16:59:25 +0200

    ver 0.1

---
+++ projekt.R
@@ -0,0 +1,125 @@
+#"D:\R-4.1.3\bin\R.exe" CMD BATCH --vanilla "--args dane.csv" projekt.R
+
+#args = commandArgs(trailingOnly=TRUE)
+#if (length(args)==0) {
+#  stop("Nie podano argumentow.")
+#}
+#dataRaw <- read.csv2(file=args[1], header=TRUE)
+
+library(Hmisc)
+library(dplyr)
+library(stats)
+library(purrr)
+
+dataRaw <- read.csv2("dane.csv")
+setwd("D:/Studia/IV Semestr/R/Projekt")
+cn <- names(dataRaw)
+groupsNames <- c()
+
+for (i in 1:length(dataRaw)){
+  if(cn[i] == "grupa" | cn[i] == "group"){
+    grID <- i
+    for(j in 1:nrow(dataRaw)){
+      if(!(dataRaw[j, i] %in% groupsNames)){
+        groupsNames <- append(groupsNames, c(dataRaw[j, i]))
+      }
+    }
+    break
+  }
+}
+
+#usuniecie brakow i wypisanie wartosci odstajacych
+
+dataModified <- dataRaw
+
+sink("zmiany.txt")
+for(i in 1:length(dataRaw)){
+  if(any(is.na(dataRaw[, i]))){
+    cat("W kolumnie ", i, "wystepuja wartosci puste w wierszach ")
+    cat(which(is.na(dataRaw[, i])))
+    cat(".\nUzupelniono srednia wynoszaca ", mean(dataRaw[, i], na.rm = TRUE), "\n\n")
+  } #else {
+    #cat("W kolumnie ", i, "nie wystepuja zadne wartosci puste \n")
+  #}
+  dataModified[, i] <- impute(dataRaw[, i], mean)
+}
+
+
+for(i in 1:length(dataModified)){
+  if(is.numeric(dataModified[, i])){
+    cat("\nWartosci odstajace w kolumnie ", cn[i], "\n")
+    cat(boxplot.stats(dataModified[, i])$out)
+    if(!length(boxplot.stats(dataModified[, i])$out)){
+      cat("Brak wartosci odstajaych\n")
+    }
+  }
+}
+
+sink()
+
+pdf("Wartosci odstajace.pdf")
+for(i in 1:length(dataModified)){
+  if(is.numeric(dataModified[, i])){
+    boxplot(dataModified[, i], main = c("wartosci odstajace w ", cn[i]))
+  }
+}
+dev.off()
+
+#charakterystyka danych
+sink("Opis danych.txt")
+ch <- data.frame(row.names = c("rodzaj", "typ", "maksymalna wartosc", "minimalna wartosc"))
+for(i in 1:length(dataModified)){
+  ch[1, i] = class(dataModified[1, i])
+  if(is.numeric(dataModified[1, i])){
+    ch[2, i] = "Dane ilosciowe"
+    ch[3, i] = max(dataModified[, i])
+    ch[4, i] = min(dataModified[, i])
+  } else {
+    ch[2, i] = "Dane jakosciowe"
+    ch[3, i] = "Nie dotyczy"
+    ch[4, i] = "Nie dotyczy"
+  }
+}
+colnames(ch) = cn
+sink()
+
+#podsumowanie danych
+sink("podsumowanie.txt")
+cat("Podsumowanie statystyk dla poszczegolnych grup bez podzialu na plec\n")
+for(i in 1:length(dataModified)){
+  if(!is.numeric(dataModified[, i])){
+    next
+  }
+  cat("\n", cn[i], "\n")
+  summary <- group_by(dataModified, dataModified[grID]) %>%
+    summarise(
+      count = n(),
+      mean = mean(wiek)
+    )
+  print(summary)
+  cat("\n")
+}
+
+cat("\nPodsumowanie statystyk dla poszczegolnych grup z podzialem na plec\n")
+
+genderID <- which(cn == "plec" | cn == "gender" | cn == "sex")
+
+for(i in 1:length(dataModified)){
+  if(!is.numeric(dataModified[, i])){
+    next
+  }
+  cat("\n", cn[i], "\n")
+  summary <- group_by(dataModified, dataModified[, grID], dataModified[, genderID], .add = TRUE) %>%
+    summarise(
+      count = n(),
+      mean = format(mean(dataModified[, i]), nsmall = 2),
+      max = format(max(dataModified[, i]), nsmall = 2),
+      min = format(min(dataModified[, i]), nsmall = 2),
+      sd = format(sd(dataModified[, i]), nsmall = 2),
+      median = format(median(dataModified[, i]), nsmall = 2),
+      iqr = format(IQR(dataModified[, i]), nsmall = 2),
+    )
+  print(summary)
+  cat("\n")
+}
+sink()
